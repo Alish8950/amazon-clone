23e0a1365a30690bb074c5c48f3662dc50776d96
Alish <alishallawadhi5@gmail.com>
2023-04-11 20:17:20 +0530

    first-commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgu56xez6nz4_UsljQymQ8liM8EeET41s",
+    authDomain: "clone-448a7.firebaseapp.com",
+    projectId: "clone-448a7",
+    storageBucket: "clone-448a7.appspot.com",
+    messagingSenderId: "1082691106534",
+    appId: "1:1082691106534:web:b7060152d488bc576a2d27",
+    measurementId: "G-5D38KND6NZ"
+  };
